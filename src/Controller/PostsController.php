fcfeb9a645895cf0e4d2712166dc457e4d6e4f40
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-08-24 10:22:47 +0200

    Editando/Actualizando Posts

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -36,6 +36,31 @@ class PostsController
 
         $view = new View('admin/posts/new.phtml');
         $view->users = (new User(Connection::getInstance()))->findAll('id, first_name, last_name');
+
+        return $view->render();
+    }
+
+    public function edit($id = null)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = $_POST;
+            $data['id'] = $id;
+            
+            $post = new Post(Connection::getInstance());
+
+            if (!$post->update($data)) {
+                Flash::add('error', 'Erro ao actualizar postagem!');
+                return header('Location: ' . HOME . '/posts/edit/' . $id);
+            }
+
+            Flash::add('success', 'Postagem actualizada com sucesso!');
+            return header('Location: ' . HOME . '/posts/edit/' . $id);
+        }
+
+        $view = new View('admin/posts/edit.phtml');
+        $view->post = (new Post(Connection::getInstance()))->find($id);
+        $view->users = (new User(Connection::getInstance()))->findAll('id, first_name, last_name');
+
         return $view->render();
     }
 }
